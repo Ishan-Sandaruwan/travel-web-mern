5c2298852438d4a4200f6d5b09e14b8d623d1aad
Ishan-Sandaruwan <sadaruwan0427@gmail.com>
2024-03-25 10:03:09 +0530

    header updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { Button } from "flowbite-react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { PiSignInBold } from "react-icons/pi";
 import { HiBars3BottomRight } from "react-icons/hi2";
 
 export default function Header() {
   const links = ["Home", "Discover", "Trips", "Review", "More"];
   const path = useLocation().pathname;
+  const navigate = useNavigate();
   const [clicked, setClicked] = useState(false);
 
   return (
@@ -29,26 +30,32 @@ export default function Header() {
             </span>
           ))}
         </div>
-        <Button gradientDuoTone="greenToBlue" outline className="absolute right-14 md:static md:right-0">
+        <Button
+          gradientDuoTone="greenToBlue"
+          outline
+          className="absolute right-14 md:static md:right-0"
+          onClick={() => {
+            navigate("/signin");
+          }}
+        >
           Sign in
           <PiSignInBold className="text-lg ml-2" />
         </Button>
-        
+
         <HiBars3BottomRight
-          className={`md:hidden inlin text-2xl ${clicked && 'transition-transform rotate-180'}`}
+          className={`md:hidden inlin text-2xl ${
+            clicked && "transition-transform rotate-180"
+          }`}
           onClick={() => setClicked(!clicked)}
         />
 
-
-
-
         {/* mobile view menu bar */}
         {clicked && (
-          <div className="absolute flex flex-col gap-4 bg-emerald-400 top-16 w-full shadow-lg shadow-emerald-300">
+          <div className="absolute flex flex-col text-white top-16 w-full shadow-lg shadow-emerald-300">
             {links.map((link, index) => (
               <span
                 key={index}
-                className={`w-full border-b-2 text-center border-white ${
+                className={`w-full border-b-2 text-center border-white bg-emerald-500 h-8 hover:bg-emerald-600 transition-transform ${
                   path === "/" + link ? "font-bold" : ""
                 }`}
               >
@@ -57,8 +64,6 @@ export default function Header() {
             ))}
           </div>
         )}
-
-
       </div>
     </div>
   );
