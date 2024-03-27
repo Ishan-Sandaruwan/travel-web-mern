cf876a40e8d6c56350b05ac9f21d68191b360aa2
Ishan-Sandaruwan <sadaruwan0427@gmail.com>
2024-03-27 18:47:23 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userShema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userShema);
+
+export default User;
